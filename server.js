72819f52d480a36a2fc08c34dfcfbbb55d3db529
SteveJDeveloper <103290510+SteveJDeveloper@users.noreply.github.com>
2024-06-27 15:10:25 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,21 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 const cors = require('cors');
 
+const domainList = ['http://localhost:4200', 'http://localhost:4201', 'http://localhost:4202', 'https://tribu-ti-nuevascapacidades-dev-gudkbwefdhd9fmhv.z01.azurefd.net']
+ 
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (domainList.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+}
+
 server.use(jsonServer.bodyParser)
 server.use(middlewares)
-server.use(cors())
+server.use(cors(corsOptions))
 
 server.use(router)
 server.listen(8181, () => {
