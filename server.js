e97929b4f4c01a4f48d7e5250fc03ad9cddf7f8f
SteveJDeveloper <103290510+SteveJDeveloper@users.noreply.github.com>
2024-06-27 14:29:39 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const cors = require('cors');
 
 server.use(jsonServer.bodyParser)
 server.use(middlewares)
+server.use(cors())
 
 server.use(router)
 server.listen(8181, () => {
